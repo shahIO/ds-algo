6dd3a6b243dff7f880a2cac1bf7d02a1afb27e14
shah <shah.sync@outlook.com>
2025-01-06 00:32:20 +0530

    Intuation: IsKthBitIsSet_Naive

--- DSALGO/BitMagic/Bit_2_KthBitIsSetOrNot.cs
+++ DSALGO/BitMagic/Bit_2_KthBitIsSetOrNot.cs
@@ -8,6 +8,12 @@
 // 5 = 101; and 3 bit is set
 // True
 
+//Naive
+// TO check if a bit b is 1 or not, use & operator => 0 & 1 = 0; 1 & 1 = 1
+// Now to check for n if kth bit is 1 or 0, use & oprator with number whose kth bit is 1 and reset is 0
+// Eg. if k = 5, then perform '&' operation with number 1000 i.e. 8
+// n = 18, k = 5 then (18 & 8)
+
 namespace DS_2_BitMagic
 {
     public class Bit_2_KthBitIsSetOrNot()
