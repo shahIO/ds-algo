08bd480146f21883757434f87cba8476596be77d
shah <shah.sync@outlook.com>
2025-01-08 23:52:48 +0530

    find odd occurance - time complexity

--- DSALGO/BitMagic/Bit_5_FindOddOccuring.cs
+++ DSALGO/BitMagic/Bit_5_FindOddOccuring.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Naive
+// TIme Complexity O(n*n)
 
+//Optimize
+// Time Complexity O(n)
 namespace DS_2_BitMagic
 {
     public class Bit_5_FindOddOccuring
